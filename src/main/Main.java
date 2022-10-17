71a37baf49deeef7dfeee5d3d523367c6b641993
Luis <lmr.87@protonmail.ch>
2022-10-18 00:31:42 +0200

    a

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,2 +1,12 @@
-package main;public class main {
+package main;
+
+public class Main {
+    public static void main(String args[]) {
+        // Creamos un objeto Saludo (Saludo extiende de Thread, tiene sus mismos métodos)
+        Saludo hilo1 = new Saludo();
+
+        System.out.println("Creando un nuevo hilo desde el hilo principal (main).");
+        hilo1.start();
+    }
+
 }
--- src/main/Saludo.java
+++ src/main/Saludo.java
@@ -1,2 +1,11 @@
-package main;public class Saludo {
+package main;
+
+public class Saludo extends Thread {
+    // Hace falta sobreescribir el metodo run
+    @Override
+    public void run() {
+        System.out.println("Saludo desde el hilo1!");
+    }
+
+
 }
